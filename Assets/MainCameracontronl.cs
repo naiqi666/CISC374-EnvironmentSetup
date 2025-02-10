4eea0e9f1aecf50cd908384aa1a024bf06427ef8
JUNPUYIN WEI NAME <naiqi666@gmail.com>
2025-02-10 09:51:50 -0500

    Added Debug.Log statement for Hello World

---
+++ Assets/MainCameracontronl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameracontronl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Why do programmers prefer dark mode? Because light attracts bugs!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
